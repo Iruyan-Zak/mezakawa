89d0bcd0d69b3b122513712d3e08b3420cf9c32a
Iruyan_ZAK <yuri.kazuna@gmail.com>
2021-01-27 03:41:54 +0900

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,17 @@
 	let analyser;
 	let bufferLength;
 	let dataArray;
-	const WIDTH = 500;
-	const HEIGHT = 200;
+	const WIDTH = 640;
+	const HEIGHT = 320;
 	const initialize = async () => {
 		try {
 			audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 			analyser = audioCtx.createAnalyser();
 			analyser.fftSize = 32768;
+			analyser.minDecibels = -120;
+			analyser.maxDecibels = 0;
+			analyser.smoothingTimeConstant = 0;
+			console.log(analyser);
 			bufferLength = analyser.frequencyBinCount;
 			dataArray = new Uint8Array(bufferLength);
 			const stream = await navigator.mediaDevices.getUserMedia({
@@ -17,20 +21,16 @@
 			});
 			audioCtx.createMediaStreamSource(stream).connect(analyser);
 		} catch (err) {
-			errorMessage.textContent = err.toString();
+			throw err.toString();
 		}
 	};
 
 	let canvas_waveTime;
-	let canvas_waveFreqency;
 	const startRenderWaveTime = () => {
 		const ctx = canvas_waveTime.getContext("2d");
-		let frame;
-		loop();
-
+		requestAnimationFrame(loop);
 		function loop(t) {
-			console.log(t);
-			frame = requestAnimationFrame(loop);
+			requestAnimationFrame(loop);
 			analyser.getByteTimeDomainData(dataArray);
 			ctx.fillStyle = "rgb(200, 200, 200)";
 			ctx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -53,36 +53,64 @@
 		}
 	};
 
+	let canvas_waveFreqency;
+
+	const leftEnd_x = 40;
+	const leftEnd_log = Math.log10(leftEnd_x);
+	const rightEnd_log = Math.log10(32768 / 2);
+	const plotPointCount = WIDTH / 2;
+	const dx_log = (rightEnd_log - leftEnd_log) / plotPointCount;
+	const plotPointUpperLimits = [];
+	for (let i = 1; i <= plotPointCount; i++) {
+		plotPointUpperLimits.push(
+			Math.floor(Math.pow(10, leftEnd_log + i * dx_log))
+		);
+	}
+	console.log(plotPointUpperLimits);
+
 	const startRenderWaveFreqency = () => {
 		const ctx = canvas_waveFreqency.getContext("2d");
-		let frame = requestAnimationFrame(loop);
-
+		requestAnimationFrame(loop);
 		function loop(t) {
-			frame = requestAnimationFrame(loop);
+			requestAnimationFrame(loop);
+
 			analyser.getByteFrequencyData(dataArray);
 			ctx.fillStyle = "rgb(200, 200, 200)";
 			ctx.fillRect(0, 0, WIDTH, HEIGHT);
 			ctx.lineWidth = 2;
 			ctx.strokeStyle = "rgb(0, 0, 0)";
 			ctx.beginPath();
-			let sliceWidth = (WIDTH * 1.0) / bufferLength;
-			let peak = 0;
-			let peaki = 0;
-			for (let i = 0; i < bufferLength; i++) {
-				let v = dataArray[i] / 256.0;
-				let x = i * sliceWidth;
-				let y = (1 - v) * HEIGHT;
-				if (i === 0) {
-					ctx.moveTo(x, y);
-				} else {
-					ctx.lineTo(x, y);
+			ctx.moveTo(0, HEIGHT - 32);
+
+			let lower = leftEnd_x;
+			let plotPoint_x = 0;
+			for (const upper of plotPointUpperLimits) {
+				let sum = 0.0;
+				for (let x = lower; x <= upper; x++) {
+					sum += dataArray[x];
 				}
-				if (peak < v) {
-					peak = v;
-					peaki = i;
-				}
+				const average = sum / (upper - lower + 1);
+				lower = upper + 1;
+
+				ctx.lineTo(plotPoint_x, 256 - average + 32);
+				plotPoint_x += 2;
 			}
-			ctx.lineTo(WIDTH, HEIGHT);
+
+			// for (let i = 0; i < bufferLength; i++) {
+			// 	let v = dataArray[i] / 256.0;
+			// 	let x = i * sliceWidth;
+			// 	let y = (1 - v) * HEIGHT;
+			// 	if (i === 0) {
+			// 		ctx.moveTo(x, y);
+			// 	} else {
+			// 		ctx.lineTo(x, y);
+			// 	}
+			// 	if (peak < v) {
+			// 		peak = v;
+			// 		peaki = i;
+			// 	}
+			// }
+			ctx.lineTo(WIDTH, HEIGHT - 32);
 			ctx.stroke();
 			// let hz = Math.round((peaki / bufferLength) * 24000);
 		}
@@ -90,30 +118,34 @@
 
 	// draw an oscilloscope of the current audio source
 
-	let systemStarted = Promise.reject("開始してください。");
-	const onClickStart = () => {
+	let systemStarted;
+	const startSystem = () => {
 		systemStarted = initialize();
 	};
+	const resetSystem = () => {
+		systemStarted = Promise.reject("開始してください。");
+	};
+	resetSystem();
 </script>
 
 <main>
 	<div>
 		{#await systemStarted then _}
 			<div>
-				<button>stop</button>
+				<button on:click={resetSystem}>stop</button>
 			</div>
 			<div>
 				<canvas
 					bind:this={canvas_waveTime}
 					on:click={startRenderWaveTime}
-					width={500}
-					height={200}
+					width={WIDTH}
+					height={HEIGHT}
 				/>
 				<canvas
 					bind:this={canvas_waveFreqency}
 					on:click={startRenderWaveFreqency}
-					width={500}
-					height={200}
+					width={WIDTH}
+					height={HEIGHT}
 				/>
 			</div>
 		{:catch message}
@@ -123,7 +155,7 @@
 					message: {message}
 				</p>
 				<div>
-					<button on:click={onClickStart}>start</button>
+					<button on:click={startSystem}>start</button>
 				</div>
 			</div>
 		{/await}
